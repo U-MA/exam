21fc878e1482e11049467045e1ca4fcb3f4ab9bc
U-MA <g.0224incident@gmail.com>
2013-08-05 10:12:42 +0900

    テストコードを下に追いやった

--- h21_b5.c
+++ h21_b5.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 
 /* n個の整数の系列が与えられたとき、その先頭要素を基準にし、その要素より小さい要素
  * すべてをその前半に、それ以外の要素をその後半にもつ系列を作成する操作を実現する
  *
- * クイックソートで使われる分割だと思われる
+ * (クイックソートで使われる分割のようなものだと思われる)
  */
 
 
@@ -13,7 +16,7 @@
  *
  *         [解答]
  *         最悪の場合、0番目の要素とn-1番目の要素を入れ替え、1番目の要素とn-2番目の要素を入れ替え,...
- *         これをO(n/2)回繰り返す。また入れ替え操作はO(1)なので、この関数の最悪時間計算量はO(n).
+ *         これをn/2回繰り返す。また入れ替え操作はO(1)なので、この関数の最悪時間計算量はO(n).
  */
 void array_partition(int *a, int n) {
 	int l, r;
@@ -53,8 +56,9 @@ typedef struct list {
 
 
 LIST *llist_partition(LIST *list) {            // llistはlinked listの略
+	LIST *tmp;
 	LIST *pivot;                                 // listの最初のセルを指す
-	LIST *l, g;                                  // little, greaterの略. それぞれlistの0番目より小さい系列の先頭,
+	LIST *l, *g;                                  // little, greaterの略. それぞれlistの0番目より小さい系列の先頭,
                                                // 以上の系列の最後を指す
 	l = g = pivot = list;
 	list = list->next;
@@ -75,23 +79,117 @@ LIST *llist_partition(LIST *list) {            // llistはlinked listの略
 }
 
 
+/****************************************************************************************************/
+/*                                                                                                  */
+/*                                    以下はテストコード                                            */
+/*                                                                                                  */
+/****************************************************************************************************/
 
-/* テストコード */
+
+
+void test_array_partition();
+void test_llist_partition();
+
+
 int main(int argc, char **argv) {
-	int a[7] = { 18, 90, 9, 5, 21, 11, 7 };
-	int i, n = 7;
+	test_array_partition();
+	test_llist_partition();
+	return 0;
+}
 
+
+
+
+void print_array(int *a, int n) {
+	int i;
 	for (i=0; i < n; i++) {
 		printf("%d ", a[i]);
 	}
 	putchar('\n');
+}
+
+
+/* array_partitionのテストコード */
+void test_array_partition() {
+	int a[7] = { 18, 90, 9, 5, 21, 11, 7 };
+	int i, n = 7;
+	int pivot;
+
+	pivot = a[0];
 
 	array_partition(a, n);
 
+	i=-1;
+	while (a[++i] < pivot);                         // pivot以上の要素を指している
+
+	printf("test_array_partition: ");
+	for (; i < n; i++) {
+		if (a[i] < pivot) {
+			printf("FAIL\n\narray: ");
+			print_array(a, n);
+			return ;
+		}
+	}
+	printf("OK\n");
+}
+
+
+LIST *insertf(LIST *list, int a);
+void print_list(LIST *list);
+
+void test_llist_partition() {
+	int a[7] = { 7, 11, 21, 5, 9, 90, 18 }, n = 7;
+	int i, pivot;
+	LIST *list, *head;
+
+	printf("test_llist_partition: ");
+
+	list = head = NULL;
 	for (i=0; i < n; i++) {
-		printf("%d ", a[i]);
+		list = insertf(list, a[i]);
+	}
+
+	pivot = list->value;
+	list = llist_partition(list);
+
+	head = list;                                    // リストの先頭を指す
+	list = list->next;
+	while ((list != NULL) && (list->value < pivot)) {
+		list = list->next;
+	}
+	if (list == NULL) {
+		printf("FAIL\nlist is null\n");
+		return ;
+	}
+
+
+	while (list != NULL) {
+		if (list->value < pivot) {
+			printf("FAIL\n\nlist: ");
+			print_list(head);
+			return ;
+		}
+		list = list->next;
+	}
+	printf("OK\n");
+}
+
+
+// 平成24年度秋B5の問題から拝借
+LIST *insertf(LIST *list, int a) {
+	LIST *newcell;
+
+	newcell = (LIST *)malloc(sizeof(LIST));
+	newcell->value = a;
+	newcell->next  = list;
+	return newcell;
+}
+
+
+void print_list(LIST* list) {
+	while (list != NULL) {
+		printf("%d ", list->value);
+		list = list->next;
 	}
 	putchar('\n');
-
-	return 0;
 }
