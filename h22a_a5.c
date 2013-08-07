8ed65adac5761fffbf39b2d0c1e96d2827ce5eb1
U-MA <g.0224incident@gmail.com>
2013-08-08 00:34:00 +0900

    count 関数を変更. テストコードをソース下部に移動.

--- h22a_a5.c
+++ h22a_a5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 // 問題(1)
@@ -13,23 +14,22 @@ void print(int x) {
 }
 
 
+// 問題(2)
 /* count: 整数xを受け取りxの10進数の各桁を調べて配列aの0番目の要素に0の出現回数,
  *        1番目の要素に1の出現回数,...,9番目に9の出現回数を格納する
- * 問題(2)
  */
 void count(int x, int *a) {
-	int i;
-	for (i=0; i < 5; i++) {
+	do {
 		a[x%10]++;
 		x /= 10;
-	}
+	} while (x != 0);
 }
 
 
+// 問題(3)
 /* countAll: 整数の配列yとその要素数nを受け取り,yのすべての要素の10進数のすべての
  *           桁を調べて、配列bの0番目の要素に0の出現回数,...,9番目の要素に9の出現回数
  *           を格納する
- * 問題(3)
  */
 void countAll(int *y, int n, int *b) {
 	int i;
@@ -37,6 +37,41 @@ void countAll(int *y, int n, int *b) {
 		count(y[i], b);
 	}
 }
+
+
+void count_test();
+
+// 問題(4)
+/* 与えられた整数の各桁に現れる0から9の出現回数を表示
+ *
+ */
+int main(int argc, char **argv) {
+	int a[10] = {
+		74459, 90037, 13510, 50310, 22576,
+		59387, 87082, 40200, 62185, 37621
+	};
+	int b[10] = { 0 };
+	int i;
+
+	count_test();
+
+	countAll(a, 10, b);
+
+	printf("各数字の出現回数を表示\n");
+	for (i=0; i < 10; i++) {
+		printf("[%d]: %d\n", i, b[i]);
+	}
+	return 0;
+}
+
+
+
+
+/***************************************************************************/
+/*                                                                         */
+/*                          以下はテストコード                             */
+/*                                                                         */
+/***************************************************************************/
 
 
 void print_test() {
@@ -48,36 +83,19 @@ void print_test() {
 
 void count_test() {
 	int n12345 = 12345;
-	int n10000 = 10000;
+	int n54321 = 54321;
 	int a[10] = { 0 };
+	int chk[10] = { 0, 2, 2, 2, 2, 2, 0, 0, 0, 0 };
 	int i;
 
-	count(n10000, a);
+	count(n12345, a);
+	count(n54321, a);
 
 	for (i=0; i < 10; i++) {
-		printf("a[%d] = %d\n", i, a[i]);
+		if (a[i] != chk[i]) {
+			printf("FAIL\n");
+			exit(1);
+		}
 	}
-}
-
-/* 与えられた整数の各桁に現れる0から9の出現回数を表示
- *
- * 問題(4)
- */
-int main(int argc, char **argv) {
-	int a[10] = {
-		74459, 90037, 13510, 50310, 22576,
-		59387, 87082, 40200, 62185, 37621
-	};
-	int b[10] = { 0 };
-	int i;
-
-	printf("print test\n");
-
-	countAll(a, 10, b);
-
-	printf("各数字の出現回数を表示\n");
-	for (i=0; i < 10; i++) {
-		printf("[%d]: %d\n", i, b[i]);
-	}
-	return 0;
+	printf("OK\n");
 }
