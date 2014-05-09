b7dd0086f4136d469a534d563db20360b1a7d3d4
U-MA <g.0224incident@gmail.com>
2014-05-09 23:41:08 +0900

    add the solution of h18f4

---
+++ h18_f4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+const int MAX = 100;
+
+/* Solution of (1) */
+void reada(int a[][MAX], const int m, const int n)
+{
+    int i, j;
+    for (i=0; i < m; i++)
+        for (j=0; j < n; j++)
+            scanf("%d", &a[i][j]);
+}
+
+/* Solution of (2) */
+void suma(int a[][MAX], const int m, const int n, int *r, int *c)
+{
+    /* initialize arrays r, c by 0 */
+    memset(r, 0, m * sizeof(int));
+    memset(c, 0, n * sizeof(int));
+
+    int i,j;
+    for (i=0; i < m; i++)
+        for (j=0; j < n; j++) {
+            r[i] += a[i][j];
+            c[j] += a[i][j];
+        }
+}
+
+/* Soluation of (3) */
+int main(int argc, char **argv)
+{
+    const int m = 3;
+    const int n = 4;
+    int a[m][n];
+    reada(a, m, n);
+
+    int r[m], c[n];
+    suma(a, m, n, r, c);
+
+    int i;
+    for (i=0; i < m; i++)
+        printf("row %d: %d\n", i, r[i]);
+    for (i=0; i < n; i++)
+        printf("col %d: %d\n", i, c[i]);
+
+    return 0;
+}
